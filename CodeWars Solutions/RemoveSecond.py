a3bc384682557ed4a5f5d7fc347ef8a1565794ca
Zayn A. <165022269+zayn-anwar@users.noreply.github.com>
2025-04-23 18:47:37 +0300

    Create RemoveSecond.py

---
+++ CodeWars Solutions/RemoveSecond.py
@@ -0,0 +1,2 @@
+# Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.def remove_every_other(my_list):
+return my_list[::2]
