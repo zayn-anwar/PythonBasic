a47e7cf6f0003d43239633e5253923eac735f18e
Zayn A. <165022269+zayn-anwar@users.noreply.github.com>
2025-04-24 22:35:53 +0300

    Create HoopChecker.py

---
+++ CodeWars Solutions/HoopChecker.py
@@ -0,0 +1,4 @@
+# Send one string depending on whether he has more than 10 hoops, another if he has less
+
+def hoop_count(n):
+    return "Great, now move on to tricks" if n>=10 else "Keep at it until you get it"
